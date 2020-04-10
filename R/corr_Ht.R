b4bbdd995c22a59f554a70a96ae75a04ca333632
Mutaz Jaber <mutazjab94@gmail.com>
2020-04-10 12:34:45 -0500

    corrected height calculation

---
+++ R/corr_Ht.R
@@ -0,0 +1,23 @@
+#' Corrected Height calculation
+#'
+#' This function calculate the corrected height
+#'
+#'
+#'
+#' (Corrected Height SDS = Ht SDS - Target Height SDS)
+#'
+#' @param z_score Taregt height z-score
+#' @param z_ht Actual height z-score
+#'
+#' @export corr_ht
+#'
+
+corr_ht <- function(z_score=NULL
+                    , z_ht=NULL){
+  if(!(z_score && z_ht)){
+    error("Z scores are required")
+  }
+
+  corr <- z_ht - z_score
+  return(cor)
+}
