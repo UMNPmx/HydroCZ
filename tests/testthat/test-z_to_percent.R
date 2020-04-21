e6f7cc4736c35a3ebd25659cc19275cecdbaf5a3
Mutaz Jaber <mutazjab94@gmail.com>
2020-04-20 21:48:31 -0500

    tests

--- tests/testthat/test-z_to_percent.R
+++ tests/testthat/test-z_to_percent.R
@@ -1,4 +1,6 @@
 test_that("percent works!", {
-  expect_equal(z_to_percent(0), "50.00%")
-  expect_equal(z_to_percent(0.01), "50.40%")
+  expect_equal(z_to_percent(0),    cat("Percentage:", "50.00%"))
+  expect_equal(z_to_percent(0.01), cat("Percentage:", "50.40%"))
+  expect_equal(z_to_percent(-0.1), cat("Percentage:","46.02%"))
+  expect_equal(z_to_percent(0.1),  cat("Percentage:", "53.98%"))
 })
